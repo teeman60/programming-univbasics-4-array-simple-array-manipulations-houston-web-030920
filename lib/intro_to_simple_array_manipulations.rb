ab8d19ca62f12db683c370c70e5e801d98170153
Taiye Salami <taiye41@gmail.com>
2020-02-15 16:04:00 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push(Array, String)
+  Array.push(String)
+end
+
+def using_unshift(Array, String)
+  Array.unshift(String)
+end
+
+def using_pop(Array)
+  Array.pop
+end
+
+def pop_with_args(Array)
+  Array.pop(2)
+end
+
+def using_shift(Array)
+  Array.shift
+end
+
+def shift_with_args(Array)
+  Array.shift(2)
+end
+
+def using_concat(Array1, Array2)
+  Array1.concat(Array2)
+end
+
+def using_insert(Array, new_element)
+  Array.insert(4, new_element)
+end
+
+def using_uniq(Array)
+  Array.uniq
+end
+
+def using_flatten(Array)
+  Array.flatten
+end
+
+def using_delete(Array, String)
+  Array.delete(String)
+end
+
+def using_delete_at(Array, Integer)
+  Array.delete_at(Integer)
+end
+  
